055b7e341f31e1b2edbaa67b5f7ae716f4ed1958
Ujjwalpreet-Singh <ujjwalpreet006@gmail.com>
2025-05-03 10:33:43 +0530

    Remove files

--- config.py
+++
@@ -1,7 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USERNAME = 'fishingapptest123@gmail.com'
-EMAIL_PASSWORD = 'kcpt npoc tlff kvnr'
