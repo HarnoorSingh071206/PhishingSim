03b1c34c0913a2ab390c9f29db3cfb2d71d7337c
Ujjwalpreet-Singh <ujjwalpreet006@gmail.com>
2025-05-05 20:31:21 +0530

    updated vercel reqs

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_USERNAME = Your_Email_Here
-EMAIL_PASSWORD = Your_Pass_Here
+EMAIL_USERNAME = 'fishingapptest123@gmail.com'
+EMAIL_PASSWORD = 'kcpt npoc tlff kvnr'
